c111d097893c420465bed2c6a135e79f1b861c31
Hemze_jx83@itstep.edu.az <Hemze_jx83@itstep.edu.az>
2023-11-07 15:21:42 +0300

    Add project files.

---
+++ BingMapLesson/Models/AllBuss.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace BingMapLesson.Models
+{
+    public static class AllBuss
+    {
+        static public ObservableCollection<AftoBus>? Buses { get; set; }
+    }
+}
---
+++ BingMapLesson/ViewModels/PageViewModels/MapPageViewModel.cs
@@ -0,0 +1,54 @@
+﻿using BingMapLesson.Models;
+using Microsoft.Extensions.Configuration;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BingMapLesson.ViewModels.PageViewModels;
+
+public class MapPageViewModel : INotifyPropertyChanged
+{
+    private ObservableCollection<AftoBus> buses;
+    public ObservableCollection<AftoBus> Buses { get => buses; set { buses = value; OnPropertyChanged(); } }
+    public MapPageViewModel()
+    {
+        //// Json file dan oxuma
+        var folder = new DirectoryInfo("../../../DataBase");
+
+        var fullPath = folder + "/Buses.json";
+
+        var jsonText = File.ReadAllText(fullPath);
+
+        var buses = JsonSerializer.Deserialize<BakuBus>(jsonText);
+
+
+        Buses = new ObservableCollection<AftoBus>(buses!.BUS);
+        AllBuss.Buses = Buses;
+        //ReapFromApi();
+    }
+
+    //private async Task ReapFromApi()
+    //{
+    //    // api  den oxumaq
+    //    var client = new HttpClient();
+    //    //var jsonText  = client.GetStringAsync("https://jsonplaceholder.typicode.com/posts").Result;
+    //    //var jsonText = client.GetStringAsync("https://raw.githubusercontent.com/CavidAtamoghlanov/BakuBusApi/main/bakubusApi.json").Result;
+    //    var jsonText = await client.GetStringAsync("https://raw.githubusercontent.com/CavidAtamoghlanov/BakuBusApi/main/bakubusApi.json");
+
+
+    //    var buses = JsonSerializer.Deserialize<BakuBus>(jsonText);
+
+        
+    //}
+
+
+    // ------------------------------------------------------------------------------------
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public void OnPropertyChanged([CallerMemberName] string? propertyNmae = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyNmae));
+}
